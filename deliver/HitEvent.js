58e8a5648575f9e39d50c0b1e92826fe1e2fee18
superjojo140 <plooploe@web.de>
2019-11-10 20:27:03 +0100

    Enable sourcemaps and remove js files from repo

--- deliver/HitEvent.js
+++
@@ -1,6 +0,0 @@
-class HitEvent {
-    constructor(initator, damage) {
-        this.initator = initator;
-        this.damage = damage;
-    }
-}
--- deliver/MovingSprite.js
+++
@@ -1,8 +0,0 @@
-class MovingSprite extends PIXI.Sprite {
-    constructor(texture) {
-        super(texture);
-        this.vx = 0;
-        this.vy = 0;
-        this.speed = 0;
-    }
-}
